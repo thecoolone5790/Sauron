61b19e64b59556b4b9b76fb503077c71f7918e51
shawn.thecool <shawn.thecool@kakaocorp.com>
2017-07-05 22:47:02 +0900

    shawn-test update

--- resources/shawn/study/node/basics/ex1_server.js
+++ resources/shawn/study/node/basics/ex1_server.js
@@ -6,3 +6,4 @@ http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(7070); //the server object listens on port 8080
+
